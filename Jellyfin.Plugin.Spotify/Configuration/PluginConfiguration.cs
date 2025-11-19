c9a29dab5331af7aa99ffa5daa93a65c04c8047f
Asriel Camora <asriel@camora.dev>
2025-11-19 15:02:41 -0800

    Initial Commit
    Signed-off-by: Asriel Camora <asriel@camora.dev>

--- Jellyfin.Plugin.Spotify/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.Spotify/Configuration/PluginConfiguration.cs
@@ -1,6 +1,6 @@
 using MediaBrowser.Model.Plugins;
 
-namespace Jellyfin.Plugin.Template.Configuration;
+namespace Jellyfin.Plugin.Spotify.Configuration;
 
 /// <summary>
 /// The configuration options.
